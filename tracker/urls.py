8a108ac0446c041a5ead68467a3bbebc03a330d0
udaykumar24-A <nothingelse855@gmail.com>
2026-04-01 14:11:52 +0530

    added login url

--- tracker/urls.py
+++ tracker/urls.py
@@ -6,6 +6,10 @@ app_name = "tracker"
 
 urlpatterns = [
     path("", views.dashboard, name="dashboard"),
+
     path("add/", views.add_application, name="add_application"),
+ # ✅ ADD THIS
+    path("login/", auth_views.LoginView.as_view(template_name="tracker/login.html"), name="login"),
 ]
+
 
