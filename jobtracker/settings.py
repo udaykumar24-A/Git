f5d839e66b06de3c82c5dd9d2f582a56e4a6e023
udaykumar24-A <nothingelse855@gmail.com>
2026-04-01 10:36:58 +0530

    fixed STATIC_ROOT position

--- jobtracker/settings.py
+++ jobtracker/settings.py
@@ -9,13 +9,12 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-import os
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
 # Quick-start development settings - unsuitable for production
